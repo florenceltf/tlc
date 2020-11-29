1ba2994346bac43dea0e32a102e69e507b9d53f9
Florence Li <florenceltf@gmail.com>
2020-11-29 14:18:03 +1100

    stamps update

--- script.js
+++ script.js
@@ -11,5 +11,35 @@ function updateTime() {
 
 updateTime();
 
+let number = 0;
+const stamps = [
+    "assets/flower.png",
+    "assets/dot.png"
+]
 
+const stampsTag = document.querySelector("div.stamps");
+
+const addStamps = function (x, y) {
+    const img = document.createElement("img");
+    img.setAttribute("src", stamps[number]);
+    img.style.left = (x - window.innerWidth / 2) + "px";
+    img.style.top = y + "px";
+
+    stampsTag.appendChild(img);
+
+    number = number + 1;
+    if (number > stamps.length - 1) {
+        number = 0;
+    }
+}
+
+
+document.addEventListener("click", function (event) {
+    addStamps(event.pageX, event.pageY);
+
+    // window.setTimeout(function () {
+    //     document.querySelector("div.stamps").remove();
+    // }, 5000);
+
+})
 
